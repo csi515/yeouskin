11b8fa0618138aa2b6280abf1845ffe9b38c8895
csi515 <csi515@github.com>
2025-07-19 10:28:01 +0900

    예약 관리 페이지 달력 가로 길이 확장
    - 그리드 레이아웃을 3칸에서 5칸으로 변경
    - 달력 영역을 1칸에서 2칸으로 확장
    - 예약 목록 영역을 2칸에서 3칸으로 조정
    - 더 넓은 달력으로 사용성 개선

--- src/pages/AppointmentManagement.tsx
+++ src/pages/AppointmentManagement.tsx
@@ -204,8 +204,8 @@ const AppointmentManagement: React.FC = () => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        <div className="lg:col-span-1">
+      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+        <div className="lg:col-span-2">
           <CalendarPanel
             appointments={appointments}
             customers={customers}
@@ -215,7 +215,7 @@ const AppointmentManagement: React.FC = () => {
           />
         </div>
         
-        <div className="lg:col-span-2">
+        <div className="lg:col-span-3">
           <ReservationListPanel
             appointments={appointments}
             customers={customers}
